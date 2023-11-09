b2e9e6a3a13140d086d6ac6dbe7c72a64442b287
Andrey Kotelnikov <dobreutore@yandex.ru>
2023-11-09 21:12:58 +0300

    fix: ссылка

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = 'https://api.movier.nomoredomainsrocks.ru';
-export const BASE_URL = 'http://localhost:3003';
+export const BASE_URL = 'https://api.movier.nomoredomainsrocks.ru';
+//export const BASE_URL = 'http://localhost:3003';
 
 // Проверка на ошибки
 const checkStatus = (res) => {
